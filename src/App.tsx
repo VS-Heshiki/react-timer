f50423328b36a4a6413bf5890f6a6c1909716c7f
Victor Heshiki <vs_heshiki@hotmail.com>
2023-07-22 18:05:36 -0300

    feat: add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/Global.styles'
+
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 
 export function App () {
     return (
         <ThemeProvider theme={ defaultTheme }>
-
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
             <GlobalStyles />
         </ThemeProvider>
     )
